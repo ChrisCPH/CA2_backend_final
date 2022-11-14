1aca3a5e6e8b355fb7a053bb650869b357f9e68a
ChrisCPH <christoffer221199@hotmail.com>
2022-11-14 14:36:15 +0100

    Small changes

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -24,8 +24,8 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.API_ExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(UserResource.class);
-        resources.add(FactResource.class);
+        resources.add(rest.UserResource.class);
+        resources.add(rest.FactResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
